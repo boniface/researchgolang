947ca9e8e870931ebf077e7e1422c1bd246b0bde
anthony-joyeux <a.joyeux16@gmail.com>
2017-05-17 11:55:04 +0200

    New changes

--- mongodb/client.go
+++ mongodb/client.go
@@ -1,1 +1,72 @@
-package mongodb
+package main
+
+import(
+	"fmt"
+	"time"
+	"net"
+	"gopkg.in/mgo.v2"
+	"crypto/tls"
+)
+
+type Student struct{
+	Name   string    `bson:"name"`
+	Age   uint8    `bson:"age"`
+	ID   uint8    `bson:"id"`
+}
+
+func main() {
+	const (
+		Host     = "10.47.2.8:27017"
+		Username = ""
+		Password = ""
+		Database = "Test"
+		Collection = "TestStudent"
+	)
+
+	student := Student{
+		Name:	"Anthony",
+		Age:	21,
+		ID:	0000,
+	}
+
+	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs:    []string{Host},
+		Username: Username,
+		Password: Password,
+		Database: Database,
+		DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
+			return tls.Dial("tcp", addr.String(), &tls.Config{})
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+
+	fmt.Println("Connected to %v!\n", session.LiveServers())
+
+	coll := session.DB(Database).C(Collection)
+
+	now1 := time.Now()
+	nano1 := now1.UnixNano()
+	//fmt.Println("\nTime before the insertion of the document: ", now1)
+	//fmt.Println(nano1)
+
+	err = coll.Insert(student)
+
+	now2 := time.Now()
+	nano2 := now2.UnixNano()
+	//fmt.Println("\nTime after the insertion of the document: ", now2)
+	//fmt.Println(nano2)
+
+	diff := nano2 - nano1
+	fmt.Println("\nTime to insert the document: ", diff, " ns\n")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Document inserted successfully!")
+}
