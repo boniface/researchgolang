0a45171e4bbaad5e29b5ff681f2b8365599f93ef
Boniface Kabaso <boniface@kabaso.com>
2017-05-21 09:14:03 +0200

    made Changes to the Code Layout

--- arangodb/arangoapi/arango_api.go
+++ arangodb/arangoapi/arango_api.go
@@ -1,21 +1,71 @@
 package arangoapi
 
 import "gopkg.in/kataras/iris.v6"
+import "time"
+
 
 
 
 type TimeElapsed struct {
 	Startime int64 `json:"startime"`
-	Endtime uint64 `json:"endtime"`
+	Endtime  int64 `json:"endtime"`
 	Duration int64 `json:"duration"`
 }
 
 func Read(ctx *iris.Context) {
 
 	var timer TimeElapsed
+	timer.Startime =time.Now().UnixNano()
+	CreateRecord()
+	timer.Endtime =time.Now().UnixNano()
+	timer.Duration = timer.Endtime-timer.Startime
+	ctx.JSON(iris.StatusOK, timer)
+}
 
 
+func ReadAll(ctx *iris.Context) {
+
+	var timer TimeElapsed
 
 
 	ctx.JSON(iris.StatusOK, timer)
 }
+
+func Create(ctx *iris.Context) {
+
+	var timer TimeElapsed
+	timer.Startime =time.Now().UnixNano()
+	CreateRecord()
+	timer.Endtime =time.Now().UnixNano()
+	timer.Duration = timer.Endtime-timer.Startime
+
+
+	ctx.JSON(iris.StatusOK, timer)
+}
+
+func CreateInNumbers(ctx *iris.Context) {
+
+
+
+	var timer TimeElapsed
+	timer.Startime =time.Now().UnixNano()
+
+	CreateRecord()
+
+	timer.Endtime =time.Now().UnixNano()
+	timer.Duration = timer.Endtime-timer.Startime
+
+
+	ctx.JSON(iris.StatusOK, timer)
+}
+
+
+
+func Delete(ctx *iris.Context) {
+
+	var timer TimeElapsed
+
+
+	ctx.JSON(iris.StatusOK, timer)
+}
+
--- main.go
+++ main.go
@@ -18,30 +18,34 @@ func main() {
 
         // ARANGODB
 	app.Get("/arango/read", arangoapi.Read)
-	app.Get("/arango/write", arangoapi.Read)
-	app.Get("/arango/delete", arangoapi.Read)
-	app.Get("/arango/readall", arangoapi.Read)
+	app.Get("/arango/write", arangoapi.Create)
+	app.Get("/arango/write/:number", arangoapi.CreateInNumbers)
+	app.Get("/arango/delete", arangoapi.Delete)
+	app.Get("/arango/readall", arangoapi.ReadAll)
 
 
 	// DO FOR REDIS
-	app.Get("/arango/read", arangoapi.Read)
-	app.Get("/arango/write", arangoapi.Read)
-	app.Get("/arango/delete", arangoapi.Read)
-	app.Get("/arango/readall", arangoapi.Read)
+	app.Get("/redis/read", arangoapi.Read)
+	app.Get("/redis/write", arangoapi.Read)
+	app.Get("/redis/write/:number", arangoapi.Read)
+	app.Get("/redis/delete", arangoapi.Read)
+	app.Get("/redis/readall", arangoapi.Read)
 
 
 	// DO FOR MONGO
-	app.Get("/arango/read", arangoapi.Read)
-	app.Get("/arango/write", arangoapi.Read)
-	app.Get("/arango/delete", arangoapi.Read)
-	app.Get("/arango/readall", arangoapi.Read)
+	app.Get("/mongo/read", arangoapi.Read)
+	app.Get("/mongo/write", arangoapi.Read)
+	app.Get("/mongo/write/:number", arangoapi.Read)
+	app.Get("/mongo/delete", arangoapi.Read)
+	app.Get("/mongo/readall", arangoapi.Read)
 
 
 	// DO FOR CASSANDRA
-	app.Get("/arango/read", arangoapi.Read)
-	app.Get("/arango/write", arangoapi.Read)
-	app.Get("/arango/delete", arangoapi.Read)
-	app.Get("/arango/readall", arangoapi.Read)
+	app.Get("/cassandra/read", arangoapi.Read)
+	app.Get("/cassandra/write", arangoapi.Read)
+	app.Get("/cassandra/write/:number", arangoapi.Read)
+	app.Get("/cassandra/delete", arangoapi.Read)
+	app.Get("/cassandra/readall", arangoapi.Read)
 
 	app.Listen(":8080")
 
