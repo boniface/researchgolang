96244f2c7d83a20c8f39928c2563966a921d540d
Boniface Kabaso <boniface@kabaso.com>
2017-05-21 14:49:04 +0200

    made Changes to the Code Layout

--- cassandra/cassandraconnection/cassandra_connection.go
+++ cassandra/cassandraconnection/cassandra_connection.go
@@ -1,1 +1,14 @@
 package cassandraconnection
+
+import "github.com/gocql/gocql"
+
+func GetConnection() ( *gocql.Session, error){
+	// connect to the cluster
+	cluster := gocql.NewCluster("10.47.2.151", "10.47.2.8", "10.47.2.1")
+	cluster.Keyspace = "example"
+	cluster.Consistency = gocql.Quorum
+	session, err := cluster.CreateSession()
+	return session, err
+
+
+}
